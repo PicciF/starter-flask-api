890f662a7fa4bc404b0b8a3202748bb6ecf67328
PicciF <marco.picci10@gmail.com>
2024-02-06 23:56:32 +0100

    test

--- app.py
+++ app.py
@@ -3,6 +3,10 @@ import os
 
 app = Flask(__name__)
 
+ 
+port = int(os.environ.get("PORT", 5000))
+app.run(host='https://bored-gray-magpie.cyclic.app/', port=port)
+
 @app.route('/')
 def hello_world():
     return 'Hello, world!'
